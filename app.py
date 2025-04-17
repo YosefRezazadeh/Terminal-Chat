fc6bf2338d436c51853be0340ee1b1c53bdf662d
YosefRezazadeh <syry814800@yahoo.com>
2025-04-17 20:45:54 +0330

    [add] Menu added to app

--- app.py
+++ app.py
@@ -1,31 +1,87 @@
 import os
 from dotenv import load_dotenv
 from src.api_handler import OpenAIAPIRequestHandler
-from src.chat_cache import ChatCache
+from src.db_handler import DBHandler
+from src.chat_utils import chat, create_cache_for_session
 
+
+# Load config
 load_dotenv()
 API_KEY = os.getenv("API_KEY", "")
 BASE_URL = os.getenv("BASE_URL", None)
-MODEL = os.getenv("MODEL", "")
-SYSTEM_ROLE = os.getenv("SYSTEM_ROLE", None)
-MEMORY_TOP_K = int(os.getenv("MEMORY_TOP_K", "3"))
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = int(os.getenv("DB_PORT", "27012"))
+DB_NAME = os.getenv("DB_NAME", "terminal-chatbot")
 
+# Create objects
 llm_api_request_handler = OpenAIAPIRequestHandler(API_KEY, BASE_URL)
-chat_cache = ChatCache()
+database_handler = DBHandler(DB_HOST, DB_PORT, DB_NAME)
+
+# Command loop
+current_session_id, current_session_name = None, None
 
 while True:
 
-    message = input("> ")
+    command = input("> ")
+    command_words = command.strip().split(" ")
 
-    try:
-        response = llm_api_request_handler.send_request(
-            chat_cache.get_cache() + message,
-            MODEL,
-            system_role=SYSTEM_ROLE)
+    if len(command_words) == 2 and command_words[0] == "new_chat":
+        new_session_id = database_handler.start_new_session(command_words[1])
+        if new_session_id is not None:
+            current_session_id = new_session_id
+            current_session_name = command_words[1]
 
-        response_text = response.choices[0].message.content
-        print(f"\n{response_text}\n")
+            # Start chat
+            chat(
+                new_session_id,
+                llm_api_request_handler,
+                database_handler
+            )
+        else:
+            print("FAILED TO CREATE NEW SESSION")
 
-        chat_cache.insert((message, response_text))
-    except:
-        print("CHECK YOUR INTERNET CONNECTION OR YOUR QUOTA")
+    elif len(command_words) == 2 and command_words[0] == "go_chat":
+        session_name = command_words[1]
+        all_session = database_handler.get_sessions()
+
+        # Check if session exists
+        found_session = None
+        for session in all_session:
+            if session[1] == session_name:
+                found_session = session
+                break
+        if found_session is None:
+            print(f"NO SESSION NAMED {session_name}")
+            continue
+
+        # Start chat
+        chat(
+            found_session[0],
+            llm_api_request_handler,
+            database_handler,
+            create_cache_for_session(
+                database_handler.get_session_chats(found_session[0]))
+        )
+
+    elif len(command_words) == 1 and command_words[0] == "list_sessions":
+        all_sessions = database_handler.get_all_sessions()
+
+        print("{:^50} {:^30} {:^30}".format("SESSION NAME", "CREATED DATE", "UPDATED DATE"))
+        for session in all_sessions:
+            print("{:^50} {:^30} {:^30}".format(
+                session["session_name"], session["create_date"], session["last_update_date"]))
+
+    elif len(command_words) == 1 and command_words[0] == "help":
+        print("SUPPORTED COMMANDS ARE FOLLOWING:")
+        print("1. Create a new session:\n\tnew_chat <session_name>")
+        print("2. Attach to existing session:\n\tgo_chat <session_name>")
+        print("3. List all existing sessions:\n\tlist_sessions")
+        print("4. Help to use Terminal-Chat:\n\thelp")
+        print("5. Exit back to main menu form session:\n\texit")
+        print("6. Show history of current session:\n\tshow")
+    else:
+        print("NO VALID COMMAND")
+
+
+
+
